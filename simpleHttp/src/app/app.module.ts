04318931aadd4ef38624fb1d10875fa5d4c775f3
JenHsuan <ofalpha@gmail.com>
2022-01-02 07:59:05 +0800

    Make basic request with HTP client

--- simpleHttp/src/app/app.module.ts
+++ simpleHttp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SimpleHttpComponent } from './simple-http/simple-http.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { SimpleHttpComponent } from './simple-http/simple-http.component';
     SimpleHttpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- simpleHttp/src/app/simple-http/simple-http.component.ts
+++ simpleHttp/src/app/simple-http/simple-http.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-simple-http',
-  templateUrl: './simple-http.component.html',
-  styleUrls: ['./simple-http.component.css']
+  templateUrl: './simple-http.component.html'
 })
 export class SimpleHttpComponent implements OnInit {
+  //add strictPropertyInitialization=false in tsconfig
+  data: Object;
+  loading: boolean;
+  httpClient: HttpClient;
+  constructor(private http: HttpClient) {
+    this.httpClient = http;
+  }
 
-  constructor() { }
-
+  makeBasicRequest(): void {
+    this.loading = true;
+    this.httpClient
+      .get('https://jsonplaceholder.typicode.com/posts/1')
+      .subscribe(data => {
+        this.data = data;
+        this.loading = false;
+        console.log(this.data)
+      })
+  }
   ngOnInit(): void {
   }
 
