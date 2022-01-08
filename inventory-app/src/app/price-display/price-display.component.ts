20cd5822723435ab94376db75469da9f67300a3c
JenHsuan <ofalpha@gmail.com>
2022-01-09 06:58:57 +0800

    Modify styles

--- inventory-app/src/app/price-display/price-display.component.ts
+++ inventory-app/src/app/price-display/price-display.component.ts
@@ -6,7 +6,9 @@ import {
 @Component({
   selector: 'app-price-display',
   template: `
+  <div class="price-display-container">
   <div class="price-display">\${{ price }}</div>
+  </div>
   `,
   styleUrls: ['./price-display.component.css']
 })
