eb7184286a41f3ee2eb10d2100de3ce0300bbdd0
JenHsuan <ofalpha@gmail.com>
2022-01-10 07:52:52 +0800

    init advanced-components

---
+++ advanced-components/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { InlineStyleComponent } from './styling/inline-style/inline-style.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InlineStyleComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
