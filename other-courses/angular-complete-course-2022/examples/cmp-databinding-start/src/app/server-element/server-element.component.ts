28f780a4b0cac3518f1cbddd88117c8708a6bb46
JenHsuan <ofalpha@gmail.com>
2022-01-18 19:35:13 +0800

    add lifecycle hooks

--- other-courses/angular-complete-course-2022/examples/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ other-courses/angular-complete-course-2022/examples/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { InvokeFunctionExpr } from '@angular/compiler';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,11 +8,52 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   // Disble encapsulation
   encapsulation: ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
-  constructor() { }
+export class ServerElementComponent implements
+  OnInit,
+  OnDestroy,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked{
+  @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
+  @ViewChild('heading', { static: true }) heading: ElementRef;
+  @ContentChild('contentParagraph', { static: true }) contentParagraph: ElementRef;
+
+  constructor() {
+    console.log('constructor');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+    console.log('textContent:' + this.heading.nativeElement.textContent);
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
+    console.log('textContent of Paragraph:' + this.contentParagraph.nativeElement.textContent);
+  }
+  ngDoCheck(): void {
+    console.log("ngDocheck")
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges:');
+    console.log(changes)
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit');
+    console.log('textContent:' + this.heading.nativeElement.textContent)
+    console.log('textContent of Paragraph:' + this.contentParagraph.nativeElement.textContent);
   }
 
 }
