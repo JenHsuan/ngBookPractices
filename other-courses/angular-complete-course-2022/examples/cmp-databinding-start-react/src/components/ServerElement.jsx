908949ea328c60e29c982ad24be43c0007e62431
JenHsuan <ofalpha@gmail.com>
2022-01-19 07:24:53 +0800

    add components for react lifecycle

---
+++ other-courses/angular-complete-course-2022/examples/cmp-databinding-start-react/src/components/ServerElement.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class ServerElement extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ServerElement
+
