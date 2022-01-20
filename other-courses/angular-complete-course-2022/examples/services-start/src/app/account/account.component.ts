f56481ff542ab269966de003eb5e4f54cff1972b
JenHsuan <ofalpha@gmail.com>
2022-01-20 10:40:29 +0800

    ch9.services

--- other-courses/angular-complete-course-2022/examples/services-start/src/app/account/account.component.ts
+++ other-courses/angular-complete-course-2022/examples/services-start/src/app/account/account.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from '../account.service';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
@@ -8,11 +10,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+  //@Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+  constructor(
+    private loggingService: LoggingService,
+    private accountService: AccountService) { }
 
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    //this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountService.updateStatus(this.id, status)
+    //this.loggingService.logStatusChangeTo(status)
+    //console.log('A server status changed, new status: ' + status);
   }
 }
