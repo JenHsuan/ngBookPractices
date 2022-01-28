efc367cd4a9b4c3b3bb4baffe79a65f2402ce537
JenHsuan <ofalpha@gmail.com>
2022-01-28 09:13:26 +0800

    complete form assignment

--- other-courses/angular-complete-course-2022/assignments/forms-td-assignment-start/src/app/app.component.ts
+++ other-courses/angular-complete-course-2022/assignments/forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+enum SubscriptionType {
+  Basic = 'Basic',
+  Advanced = 'Advanced',
+  Pro = 'Pro'
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultSubscription = SubscriptionType.Advanced;
+  subscriptionOptions = Object.keys(SubscriptionType)
+  submitted = false
 
+  constructor() {
+    console.log(this.subscriptionOptions)
+  }
+  onSubmit() {
+    console.log(this.signupForm)
+    this.submitted = true
+  }
 }
