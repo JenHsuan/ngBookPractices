408d46fa69948a27f5e0c21ace2e4f3bfaf78f9d
JenHsuan <ofalpha@gmail.com>
2023-05-23 20:05:19 +0800

    update the directive project

--- other-courses/angular-complete-course-2022/examples/directives/directives-start/src/app/app.module.ts
+++ other-courses/angular-complete-course-2022/examples/directives/directives-start/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-hightlight/better-highlight.directive';
--- other-courses/angular-complete-course-2022/examples/directives/directives-start/src/app/basic-highlight/basic-highlight.directive.ts
+++ other-courses/angular-complete-course-2022/examples/directives/directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,12 +1,14 @@
 import { Directive, ElementRef, OnInit } from '@angular/core';
 
-@Directive({ selector: '[appBasicHighlight]' })
+// the brace brackets [] in the selector means that thos directive can be used as a attribute
+@Directive({ 
+    selector: '[appBasicHighlight]' 
+})
 export class BasicHighlightDirective implements OnInit{
     constructor(private elementRef: ElementRef) {
+    }
+
+    ngOnInit() {
         this.elementRef.nativeElement.style.backgroundColor = 'green';
-
     }
-    ngOnInit(): void {
-    }
-
 }
